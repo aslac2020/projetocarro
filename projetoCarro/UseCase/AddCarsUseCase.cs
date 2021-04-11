1f415b61e835b4a8ce97993b243a8faf99e2ae5b
aslac2020 <andredesenvolvedorti@gmail.com>
2021-04-11 09:32:30 -0300

    alterações

--- projetoCarro/UseCase/AddCarsUseCase.cs
+++ projetoCarro/UseCase/AddCarsUseCase.cs
@@ -26,7 +26,7 @@ namespace projetoCarro.UserCase
 
             try
             {
-                if (request.Model.Length < 1)
+                if (request.Model.Length < 3)
                 {
                     response.msg = "Erro ao adicionar o carro";
                     return response;
